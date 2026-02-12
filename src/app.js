61d360dfdd12e9081f95cd82046fd4702e661342
abdunur <amangeldievabdunur@gmail.com>
2026-02-13 02:05:43 +0500

    file structure changes

--- src/app.js
+++ src/app.js
@@ -3,9 +3,12 @@ const routes = require("./routes");
 const cors = require('cors');
 const errorMiddleware = require("./middlewares/error.middleware");
 const path = require("node:path");
+const fs = require("node:fs");
 
 
 const app = express();
+const uploadsPath = path.join(__dirname, "../uploads");
+const frontendDistPath = path.join(__dirname, "../frontend/dist");
 
 app.use(express.json());
 app.use(cors({
@@ -13,11 +16,24 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
-app.use(express.static(path.join(__dirname, "../public")));
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-});
+if (!fs.existsSync(uploadsPath)) {
+    fs.mkdirSync(uploadsPath, { recursive: true });
+}
+
+app.use("/uploads", express.static(uploadsPath));
 app.use("/api", routes);
+
+if (fs.existsSync(frontendDistPath)) {
+    app.use(express.static(frontendDistPath));
+    app.get(/^(?!\/api|\/uploads).*/, (req, res) => {
+        res.sendFile(path.join(frontendDistPath, "index.html"));
+    });
+} else {
+    app.get("/", (req, res) => {
+        res.status(200).send("Frontend build not found. Run `npm run build` in ./frontend.");
+    });
+}
+
 app.use(errorMiddleware);
 
 module.exports = app;
